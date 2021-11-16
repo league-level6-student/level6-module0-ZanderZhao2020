43007095f6496cc2378cfe0f87d324f69226bf0a
League Student <league@iMac-13.attlocal.net>
2021-11-15 18:28:49 -0800

    fasdfasdf

--- src/test/java/_08_mocking/models/DeliveryDriverTest.java
+++ src/test/java/_08_mocking/models/DeliveryDriverTest.java
@@ -10,39 +10,44 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 class DeliveryDriverTest {
-
+	@Mock
     DeliveryDriver deliveryDriver;
 
     @BeforeEach
     void setUp() {
-
+    	MockitoAnnotations.openMocks(this);
+    	
+    	deliveryDriver = new DeliveryDriver("Bob", new Car(new _07_intro_to_mocking.models.Engine(69.420, 69, 69420, 420, 69), new _07_intro_to_mocking.models.GasTank()), new CellPhone());
     }
 
     @Test
     void itShouldWasteTime() {
         //given
-
+    	boolean ExpectedTimeWasted = true;
         //when
-
+    	boolean ActualTimeWasted = deliveryDriver.wasteTime();
         //then
+    	assertEquals(ExpectedTimeWasted, ActualTimeWasted);
     }
 
     @Test
     void itShouldRefuel() {
         //given
-
+    	boolean ExpectedRefueled = true;
         //when
-
+    	boolean ActualRefueled = deliveryDriver.refuel(69420);
         //then
+    	assertEquals(ExpectedRefueled, ActualRefueled);
     }
 
     @Test
     void itShouldContactCustomer() {
         //given
-
+    	boolean ExpectedContacted = true;
         //when
-
-        //then
+    	boolean ActualContacted = deliveryDriver.contactCustomer("(800)-800-8000");
+        //the
+    	assertEquals(ExpectedContacted, ActualContacted);
     }
 
 }
--- src/test/java/_10_white_box_testing/MyDonutShopTest.java
+++ src/test/java/_10_white_box_testing/MyDonutShopTest.java
@@ -1,6 +1,7 @@
 package _10_white_box_testing;
 
 import _09_intro_to_white_box_testing.models.DeliveryService;
+
 import _09_intro_to_white_box_testing.models.Order;
 import _10_white_box_testing.models.BakeryService;
 import _10_white_box_testing.models.PaymentService;
@@ -9,32 +10,52 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import _07_intro_to_mocking.models.*;
+import _08_mocking.models.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-class MyDonutShopTest {
+import java.util.ArrayList;
+import java.util.List;
 
+class MyDonutShopTest {
+	@Mock
     MyDonutShop myDonutShop;
+	
+	@Mock
+	DeliveryService deliveryService;
+	
+	@Mock
+	BakeryService bakeryService;
 
     @BeforeEach
     void setUp() {
+    	MockitoAnnotations.openMocks(this);
+    	
+
+    	myDonutShop = new MyDonutShop(null, deliveryService, bakeryService);
+    	myDonutShop.setDeliveryService(deliveryService);
 
     }
 
     @Test
     void itShouldTakeDeliveryOrder() throws Exception {
         //given
-
+    	myDonutShop.openForTheDay();
+    	Order order = new Order(null, null, 0, 0, null, false);
         //when
-
+    	myDonutShop.takeOrder(order);
         //then
+    	verify(deliveryService, times(0)).scheduleDelivery(order);
     }
 
     @Test
     void givenInsufficientDonutsRemaining_whenTakeOrder_thenThrowIllegalArgumentException() {
         //given
-
+    	myDonutShop.openForTheDay();
+    	Order order = new Order(null, null, 100, 0, null, false);
         //when
 
         //then
